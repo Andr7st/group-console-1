a271275907d5c411e67351e3ed5b5619809e907d
Andrés Segura <63387323+Andr7st@users.noreply.github.com>
2021-06-12 17:20:39 -0500

    first commit

---
+++ src/paqu/Start.java
@@ -0,0 +1,11 @@
+package paqu;
+/**
+ * @uthor Grupo
+ *
+ * ¿Si empezamos con una calculadora?
+ * */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hola grupo!!");
+    }
+}
